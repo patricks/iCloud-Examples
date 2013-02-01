24819f9e0d3b1842441204bd86895b67efe14a1d
Patrick <patrick@helmsdeep.at>
2013-02-01 10:42:40 +0100

    Added plist icloud example

---
+++ icloud-example-plist/icloud-example-plist/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  icloud-example-plist
+//
+//  Created by Patrick Steiner on 01.02.13.
+//  Copyright (c) 2013 Patrick Steiner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
